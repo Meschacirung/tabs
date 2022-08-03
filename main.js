6f42dc1c347fbb053366496fc741569d6ce62d3c
Meschac Irung <meschacirung@gmail.com>
2022-08-03 10:59:05 +0200

    add tabs behavior

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
 import "./assets/css/tailwind.css";
 
-let switchers = document.querySelectorAll(".switcher");
-let sysSwitcher = document.querySelector("#system");
+let tabs = document.querySelectorAll(".tab")
+let indicator = document.querySelector(".indicator")
+let panels = document.querySelectorAll(".tab-panel")
 
-if (
-  localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
+indicator.style.width = tabs[0].getBoundingClientRect().width + 'px'
+indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + 'px'
 
-switchers.forEach((switcher) => {
-  switcher.addEventListener("click", function () {
-    if (localStorage.getItem("color-theme")) {
-      if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
+tabs.forEach(tab =>{
+  tab.addEventListener("click", ()=>{
+    let tabTarget = tab.getAttribute("aria-controls")
+
+    indicator.style.width = tab.getBoundingClientRect().width + 'px'
+    indicator.style.left = tab.getBoundingClientRect().left - tab.parentElement.getBoundingClientRect().left + 'px'
+
+
+    panels.forEach(panel =>{
+      let panelId = panel.getAttribute("id")
+      if(tabTarget === panelId){
+        panel.classList.remove("invisible", "opacity-0")
+        panel.classList.add("visible", "opacity-100")
       } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
+        panel.classList.add("invisible", "opacity-0")
       }
-    } else {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      }
-    }
-  });
-});
+    })
+  })
+})
